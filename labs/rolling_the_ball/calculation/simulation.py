bbef8b001aaf3e22f5b60afd1d742e3230ee7ab9
kirill-tsimbaliuk <tsimbaliukk@ya.ru>
2025-10-28 19:43:50 +0300

    fixed calculator method

--- labs/rolling_the_ball/calculation/simulation.py
+++ labs/rolling_the_ball/calculation/simulation.py
@@ -1,5 +1,3 @@
-from collections.abc import Generator
-
 from ..model import Ball, Environment
 from .movement import move_with_slippage, move_without_slippage
 from .utils import is_movement_without_slippage_possible, is_slippage_finished
@@ -10,14 +8,12 @@ class Calculator:
         self.env = env
         self.current_ball = ball
 
-    def __call__(self, time_delta: float) -> Generator[Ball]:
+    def __call__(self, time_delta: float) -> Ball:
         if not is_movement_without_slippage_possible(self.env):
-            yield move_with_slippage(self.current_ball, self.env, time_delta)
-        else:
-            if is_slippage_finished(self.current_ball):
-                yield move_without_slippage(self.current_ball, self.env, time_delta)
-            else:
-                yield move_with_slippage(self.current_ball, self.env, time_delta)
+            return move_with_slippage(self.current_ball, self.env, time_delta)
+        if is_slippage_finished(self.current_ball):
+            return move_without_slippage(self.current_ball, self.env, time_delta)
+        return move_with_slippage(self.current_ball, self.env, time_delta)
 
 
 def is_simulation_finished(ball: Ball, env: Environment) -> bool:
